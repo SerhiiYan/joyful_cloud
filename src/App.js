c65372a07ec63d1e939b9f98f647d6a1eee113da
YanchevskyiSerhii <serhii.yanchevskii@gmail.com>
2022-07-04 12:04:01 +0300

    clean project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import "./styles/app.scss";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrapper">
+      <div className="App"></div>
+    </div>
+  );
 }
 
 export default App;
