16fe9020a8ac04b45bd885bc1c99b3ce75d32ca6
YanchevskyiSerhii <serhii.yanchevskii@gmail.com>
2022-07-05 10:39:24 +0300

    feat: add header, content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,42 @@ import "./styles/app.scss";
 function App() {
   return (
     <div className="wrapper">
-      <div className="App"></div>
+      <header className="header">
+        <div className="logo">
+          <span className="logo-animation">JC</span>
+          <div className="logo-fullname">
+            <span>JOYFUL</span>
+            <span>CLOUD</span>
+          </div>
+        </div>
+        <div className="navbar">
+          <nav>
+            <ul>
+              <li>JOYFUL</li>
+              <li>CLOUD</li>
+              <li>LAMA</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <div className="content">
+        <div className="main">
+          <div className="left-block">
+            <h1>SUPPORT</h1>
+            <span>LAMA NOW</span>
+            <p>Funny site about llamas</p>
+            <button>Donate</button>
+          </div>
+          <div className="right-block">
+            <img
+              width={400}
+              height="auto"
+              src="./assets/img/lama-icon.png"
+              alt="lama"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
